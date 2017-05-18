1e796530284baff4cf8befd10364e6fce8c51281
Enes K <enesmil@gmail.com>
2017-05-18 11:58:49 +0200

    Wovel count challenge perfected!

--- 12- Wovel count/challenge.js
+++ 12- Wovel count/challenge.js
@@ -1,5 +1,24 @@
-let str = "Train";
+let str = "bzz8*";
 
-str = str.match(/[a,e,i,o,u]/gi);
+let pattern = /[a,e,i,o,u]/gi;
 
-console.log(str.length);
+str = str.match(pattern);
+
+if (str === null) {
+	str = 0;
+	console.log(str);
+} else {
+	console.log(str.length);
+}
+
+/* Fully working solution */
+/*
+  str = str.toLowerCase();
+  var counter = 0;
+  for(x in str) {
+  	if(str[x] == 'a' || str[x] == 'e'|| str[x] == 'i'|| str[x] == 'o'|| str[x] == 'u') {
+      counter++;
+    }
+  }
+  return counter; 
+*/
