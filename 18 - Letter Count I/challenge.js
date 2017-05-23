921cf9bf3c36a96cc2a79a88d3b49052d738e32a
Enes K <enesmil@gmail.com>
2017-05-23 15:04:15 +0200

    Objck knowledge still not enough to solve this problem natively.

--- 18 - Letter Count I/challenge.js
+++ 18 - Letter Count I/challenge.js
@@ -1,14 +1,27 @@
 //Brek sentence to words /\b[a-z]+/gi
 let str = "Hello apple pie";
 
-arr = str.match(/\b[a-z]+/gi);
+str = str.replace(/\s/g, ""); //Remove all empty spaces.
+
+let arr = Array.from(str);
+
+let obj = {};
+
 
 for (var i = 0; i < arr.length; i++) {
-	Things[i]
+	obj[arr[i]] = 0;
 }
 
 
+for (var i = 0; i < arr.length; i++) {
+	if(obj[arr[i]] == 0) {
+		obj[arr[i]] = 1;
+	} else if (obj[arr[i]] > 0) {
+		obj[arr[i]] +=1;
+	}
+}
 
+console.log(obj);
+console.log(arr);
 
-
-console.log(str);
+//Knowledge still not good enough!!!
