dd5aacecd37552595ea0125384e95499e9d3a3f2
Vanguard90 <enesmil@gmail.com>
2017-07-25 23:41:19 +0200

    Attempt at solving the Off line minimum challenge

--- 30 - Off line minimum/challenge.js
+++ 30 - Off line minimum/challenge.js
@@ -1,21 +1,40 @@
 let strArr = ["5","4","6","E","1","7","E","E","3","2"];
 
-let orgArr = []
+//Iterate on array until E character.
+//Upon reaching E, push the smallest integer out of the current array onto a new array.
 
-let eCounter = 0;
+let eIndex = [];
 
-for (var i = 0; i < strArr.length; i++) {
-	if (strArr[i] == "E") {eCounter++;} 
+let item = 'E';
+
+var idx = strArr.indexOf(item);
+while (idx != -1) {
+  eIndex.push(idx);
+  idx = strArr.indexOf(item, idx + 1);
 }
-/*
-for (var i = 0; i <= eCounter; i++) {
-	orgArr[i] = [];
-	for (var j = 0; j < eCounter; j++) {
-	while()
-	}
+
+let minimum = [];
+
+for (var i = 0; i < eIndex.length; i++) {
+	let tempArr = strArr.slice(0,eIndex[i]);
+	tempArr = tempArr.sort(function (a,b) {
+		if (a > b) {
+			return 1;
+		} else if (b > a) {
+			return -1;
+		}
+	})
+	minimum.push(tempArr[0]);
+	let first = tempArr[0];
+	let delPoint = first;
+	console.log(delPoint);
+	//let delIndex = strArr.IndexOf(delPoint);
+	//console.log(delIndex);
+	//strArr[delIndex].pop();
 }
-*/
 
+console.log(minimum);
 
+console.log(eIndex);
 
-console.log();
+console.log(counter);
