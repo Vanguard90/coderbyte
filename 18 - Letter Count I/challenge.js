5466c8b271b55775e89185a698af4d236287c486
Vanguard90 <enesmil@gmail.com>
2017-10-06 21:42:24 +0200

    Tried solving Letter Count I again. made some progress.

--- 18 - Letter Count I/challenge.js
+++ 18 - Letter Count I/challenge.js
@@ -1,27 +1,51 @@
 //Brek sentence to words /\b[a-z]+/gi
 let str = "Hello apple pie";
 
-str = str.replace(/\s/g, ""); //Remove all empty spaces.
-
-let arr = Array.from(str);
+arr = str.split(" ");
 
 let obj = {};
 
-
 for (var i = 0; i < arr.length; i++) {
-	obj[arr[i]] = 0;
+	obj[i] = arr[i];
 }
 
+let pieces = [];
 
 for (var i = 0; i < arr.length; i++) {
-	if(obj[arr[i]] == 0) {
-		obj[arr[i]] = 1;
-	} else if (obj[arr[i]] > 0) {
-		obj[arr[i]] +=1;
+	pieces[i] = arr[i].split("");
+}
+
+console.log(pieces);
+
+let counts = {};
+
+
+for (var i = 0; i < pieces.length; i++) {
+	counts[i] = {};
+	for (var j = 0; j < pieces[i].length; j++) {
+		if (counts[i][pieces[i][j]] == undefined) {
+			counts[i][pieces[i][j]] = 1;	
+		} else if (counts[i][pieces[i][j]] =! undefined) {
+			counts[i][pieces[i][j]]++;
+		}
+	}
+	
+}
+let highest = [];
+
+for (var k = 0; k < counts.length; k++) {
+	for (deepCounter in counts[k]) {
+		if(counts[k][deepCounter] > counts[k][deepCounter]) {
+			highest[k] = counts[k][deepCounter];
+		} else {
+			highest[k] = counts[k][deepCounter];
+		}
 	}
 }
 
-console.log(obj);
-console.log(arr);
 
-//Knowledge still not good enough!!!
+//Made progress but still not correct!!!
+
+console.log(highest);
+
+console.log(counts);
--- 32 - Overlapping ranges/challenge.js
+++ 32 - Overlapping ranges/challenge.js
@@ -28,4 +28,20 @@ if (counter >= arr.pop()) {
 	console.log(false);
 }
 
+
+/*
+
+Better solution:
+
+function OverlappingRanges(arr) {
+    var target = arr.pop();
+    var MaxLowerBound = Math.max(arr[0], arr[2]);
+    var MinUpperBound = Math.min(arr[1], arr[3]);
+
+    var range = MinUpperBound - MaxLowerBound + 1;
+
+    return range >= target ? 'true' : 'false';
+}
+
+*/
 console.log(counter);
